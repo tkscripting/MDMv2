a4e7f487a94d953ca256af8346b0f8f2eb0e7e3b
tkscripting <tyler.knipping@ynap.com>
2025-07-14 13:59:06 -0400

    Update Active.js

--- Active.js
+++ Active.js
@@ -1,18 +1,16 @@
 // ==UserScript==
 // @name         Active
 // @namespace    http://tampermonkey.net/
-// @version      1.7
+// @version      1.8
 // @description  Force-trigger lazy-loaded content by faking IntersectionObserver
 // @match        https://madame.ynap.biz/*
+// @exclude      https://madame.ynap.biz/shooting-validation
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
-
 (function () {
     'use strict';
-
     const observed = new WeakSet();
-
     window.IntersectionObserver = class {
         constructor(callback) {
             this.callback = callback;
